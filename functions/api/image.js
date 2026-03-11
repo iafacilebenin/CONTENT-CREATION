833800e9dc7918aa8df96d4b18fb3332d706e635
iafacilebenin <iafacilebenin@gmail.com>
2026-03-11 05:29:20 +0100

    Update image.js

--- functions/api/image.js
+++ functions/api/image.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
       });
     }
     const hfResponse = await fetch(
-      'https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-2-1',
+      'https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell',
       {
         method: 'POST',
         headers: {
